b9044496d29a50e32a4c046f071fb69685dcc60d
Jeffrey Webb <jeff@MacBook-Pro.local>
2018-08-27 19:32:30 -0400

    letters and x to show used letters

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {ALPHABET.map((letter, i) => 
+          return <letterButton key={i} letter={letter}/>
+
+         })}
+        
       </div>
     );
   }
